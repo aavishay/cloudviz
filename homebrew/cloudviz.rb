6c7c2df693cd7f557a5c7c182f4e53b43864efa8
Avishay Ashkenazi <aavishay@gmail.com>
2026-04-16 00:35:06 +0300

    chore: rebuild frontend for v0.5.1

--- homebrew/cloudviz.rb
+++ homebrew/cloudviz.rb
@@ -2,10 +2,10 @@ class Cloudviz < Formula
   desc "Azure cloud infrastructure visualization and cost management dashboard"
   homepage "https://github.com/aavishay/cloudviz"
   license "MIT"
-  version "0.4.0"
-  sha256 "84c171089b1d5a0afa5e6a2a3e86f9c5e45c43dab2a4e74c7d0599be694279c2"
+  version "0.5.1"
+  sha256 "7c4ade196d0fed60d1c3858acbc451eb6ffcb0b541a15f9a9c6a98c87b69936d"
 
-  url "https://github.com/aavishay/cloudviz/releases/download/v0.4.0/cloudviz_0.4.0_darwin_arm64.tar.gz"
+  url "https://github.com/aavishay/cloudviz/releases/download/v0.5.1/cloudviz_0.5.1_darwin_arm64.tar.gz"
 
   def install
     bin.install "cloudviz"
