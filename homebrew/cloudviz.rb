653de64e79866df8547a3842ce38c920d501c355
Avishay Ashkenazi <aavishay@gmail.com>
2026-04-30 22:34:11 +0300

    release: bump version to v1.2.0

--- homebrew/cloudviz.rb
+++ homebrew/cloudviz.rb
@@ -2,13 +2,34 @@ class Cloudviz < Formula
   desc "Azure cloud infrastructure visualization and cost management dashboard"
   homepage "https://github.com/aavishay/cloudviz"
   license "MIT"
-  version "0.8.0"
-  sha256 "6b138f16ebf4a2a2a6b8f7759de0dc75fd59f6b7c40b2ef4ddbe2fde0295c2e1"
+  version "1.2.0"
 
-  url "https://github.com/aavishay/cloudviz/releases/download/v0.8.0/cloudviz_0.8.0_darwin_arm64.tar.gz"
+  on_macos do
+    on_arm do
+      url "https://github.com/aavishay/cloudviz/releases/download/v#{version}/cloudviz_#{version}_darwin_arm64.tar.gz"
+      sha256 "45efa90a0b8575eece7af8e5fd8d052f66f01a3a91076384af0023d9b3652f2c"
+    end
+    on_intel do
+      url "https://github.com/aavishay/cloudviz/releases/download/v#{version}/cloudviz_#{version}_darwin_amd64.tar.gz"
+      sha256 "c3f2512006966a282b7b2dd7549318f88c59d1b2107ec1cf6efec2923777afc7"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/aavishay/cloudviz/releases/download/v#{version}/cloudviz_#{version}_linux_amd64.tar.gz"
+      sha256 "657c65dcace1c4a96805c93fdadbedaffc9d72f5a7c156573e23ded6a28aef6b"
+    end
+  end
 
   def install
     bin.install "cloudviz"
+  end
+
+  def post_install
+    puts "CloudViz v1.2.0 installed!"
+    puts "Run 'cloudviz serve' to start the server."
+    puts "Run 'cloudviz --help' for all available commands."
   end
 
   test do
