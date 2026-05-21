7b813fbf575badd3afd16c97417a2e8751165a72
Avishay Ashkenazi <aavishay@gmail.com>
2026-05-22 00:20:16 +0300

    Update to v1.26.0

--- homebrew/cloudviz.rb
+++ homebrew/cloudviz.rb
@@ -2,23 +2,23 @@ class Cloudviz < Formula
   desc "Azure cloud infrastructure visualization and cost management dashboard"
   homepage "https://github.com/aavishay/cloudviz"
   license "AGPL-3.0"
-  version "1.24.1"
+  version "1.26.0"
 
   on_macos do
     on_arm do
       url "https://github.com/aavishay/cloudviz/releases/download/v#{version}/cloudviz_#{version}_darwin_arm64.tar.gz"
-      sha256 "722eee91c0c9a3d3118fe17c839edc9085f290563be75cf5da7513a8fae80e03"
+      sha256 "68126f29242dbc0840834677d36d1f320302f1abb3ac03ff0a7af9f74f689d96"
     end
     on_intel do
       url "https://github.com/aavishay/cloudviz/releases/download/v#{version}/cloudviz_#{version}_darwin_amd64.tar.gz"
-      sha256 "542febf06318ebb7ad00a9e920ff797aaecbdfba09ed009c0af68e780a50d96a"
+      sha256 "4f4b0a8256c7e28bbe215d370f49b34d8b71d0b17f042b5c1139400e1ca3067a"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/aavishay/cloudviz/releases/download/v#{version}/cloudviz_#{version}_linux_amd64.tar.gz"
-      sha256 "40c78c95a286a994ac44625ed5887fbc760ec1efef103272e3f9785a653ce00f"
+      sha256 "836a2b23476a0f0e8607887781df3aa23a5d3d0ba376bf8fb4be1122e38e75de"
     end
   end
 
